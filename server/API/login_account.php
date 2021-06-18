01b5cc56468b59816c9c86b4f8eaa9b4ac45835b
Bardia Madadi <bardiamadadi86@gmail.com>
2021-06-18 14:48:19 +0430

    delete query line 28 and use query line 28 instead

--- server/API/login_account.php
+++ server/API/login_account.php
@@ -16,37 +16,23 @@ if (isset($_POST['mail']) && isset($_POST['pwd'])) {
         //when connected to server
 
         // select user with that mail
-        $query = "SELECT * FROM `users` WHERE u_email = '$mail' AND u_pwd = '$pwd'";
+        $query = "SELECT * FROM `users` WHERE u_email = '$mail' AND u_pwd = '$pwd' LIMIT 1";
         // return number of users with that info
         $num_row = mysqli_num_rows(mysqli_query($conn, $query));
-        if ($num_row > 0) {
-            //if there is more than 0 user
-            if ($num_row == 1) {
 
 
+        $user_info = mysqli_fetch_array(mysqli_query($conn, $query), MYSQLI_ASSOC);
 
-                $query = "SELECT * FROM `users` WHERE u_email = '$mail' AND u_pwd = '$pwd' LIMIT 1";
-                // return number of users with that info
-                //
-                $user_info = mysqli_fetch_array(mysqli_query($conn, $query), MYSQLI_ASSOC);
-
-                //if there is 1 user with that info
-                //
-                if (hashing($pwd, $user_info['u_name']) == $user_info['pwd']) {
-                    //if pwd match
-                    //
-                    $data['id'] = $user_info['id'];
-                    $data['username'] = $user_info['u_name'];
-                    $data['email'] = $user_info['u_email'];
-                    response(200,"User found",$data);
-                } else {
-                    //pwd dose  not match
-                    response(250,"password dose not match",null);
-                }
-            } 
+        if (hashing($pwd, $user_info['u_name']) == $user_info['pwd']) {
+            //if pwd match
+            //
+            $data['id'] = $user_info['id'];
+            $data['username'] = $user_info['u_name'];
+            $data['email'] = $user_info['u_email'];
+            response(200, "User found", $data);
         } else {
-            //if there is not user with that info
-            response(250,"there is not user with that info",null);
+            //pwd dose  not match
+            response(250, "password dose not match", null);
         }
     }
 }
@@ -65,4 +51,3 @@ function response($status, $status_message, $data)
     $json_response = json_encode($response);
     echo $json_response;
 }
-
